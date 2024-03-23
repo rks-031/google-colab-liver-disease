f2bf943ed4cd4091f6862345c3d31b13db8c4881
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-03-23 10:28:27 +0530

    toggler fixed and model working

--- app.py
+++ app.py
@@ -19,8 +19,24 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        inputs = [int(x) for x in request.form.values()]
-        inputs = np.array([inputs])
+        Age = int(request.form['Age'])
+        Gender = int(request.form['Gender'])
+        Total_Bilirubin = float(request.form['Total_Bilirubin'])
+        Direct_Bilirubin = float(request.form['Direct_Bilirubin'])
+        Alkaline_Phosphotase = int(request.form['Alkaline_Phosphotase'])
+        Alamine_Aminotransferase = int(
+            request.form['Alamine_Aminotransferase'])
+        Aspartate_Aminotransferase = int(
+            request.form['Aspartate_Aminotransferase'])
+        Total_Protiens = float(request.form['Total_Protiens'])
+        Albumin = float(request.form['Albumin'])
+        Albumin_and_Globulin_Ratio = float(
+            request.form['Albumin_and_Globulin_Ratio'])
+
+        inputs = np.array([[Age, Gender, Total_Bilirubin, Direct_Bilirubin, Alkaline_Phosphotase,
+                            Alamine_Aminotransferase, Aspartate_Aminotransferase, Total_Protiens,
+                            Albumin, Albumin_and_Globulin_Ratio]])
+
         inputs = sc.transform(inputs)
         output = model.predict(inputs)
         if output < 0.5:
